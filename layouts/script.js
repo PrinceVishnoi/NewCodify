c60fbffc27afd4942474d8db54b078d55104c1b0
Prince Vishnoi <96827891+PrinceVishnoi@users.noreply.github.com>
2022-09-06 16:59:07 +0530

    First Commit

---
+++ layouts/script.js
@@ -0,0 +1,28 @@
+/*var btn=document.getElementById('formbutton');
+btn.addEventListener('click',function handleClick(event) {
+      // 👇️ if you are submitting a form (prevents page reload)
+      event.preventDefault();
+    
+      const Name = document.getElementById('name');
+      console.log(Name.value);
+      Name.value = '';
+
+      const email = document.getElementById('email');
+      console.log(email.value);
+      email.value = '';
+
+
+      const message = document.getElementById('message');
+      console.log(message.value);
+      message.value = '';
+
+    });*
+    function store(){
+      var inputEmail= document.getElementById("email");
+      localStorage.setItem("email", inputEmail.value);
+      var name= document.getElementById("name");
+      localStorage.setItem("name", name.value);
+      var Message= document.getElementById("message");
+      localStorage.setItem("message", Message.value);
+     }
+     */
